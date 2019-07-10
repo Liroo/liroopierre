474098b0e87144cfc575136d7dc68ba0e7bab1a1
Liroo <liroo.pierre@gmail.com>
2019-07-11 01:46:13 +0200

    Add favicon

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/jpeg" href="<?php get_template_directory() . '/public/images/favicon.jpg'; ?>">
     <?php wp_head(); ?>
   </head>
   <body>
